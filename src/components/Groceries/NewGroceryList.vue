43218bd2454549aebd1d465ae1e12dc3133755f9
qwark97 <marcin.plata1.konta@gmail.com>
2021-04-15 21:09:42 +0200

    add base for adding new grocery list

---
+++ src/components/Groceries/NewGroceryList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>
+        <h1>works</h1>
+    </div>
+    <router-link to="/groceries">
+        <NewGroceryListButton class="add" @click="addList()" :text="addListButtonText"/>
+    </router-link>
+    <router-link to="/groceries">
+      <NewGroceryListButton class="cancel" :text="cancelListButtonText"/>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import NewGroceryListButton from "./NewGroceryListButton";
+
+export default {
+  name: "NewGroceryList",
+  components: {
+    NewGroceryListButton,
+  },
+  data: function() {
+      return {
+          addListButtonText: "Add",
+          cancelListButtonText: "Cancel"
+      }
+  },
+  methods: {
+    addList() {
+      console.log('works')
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .add {
+    margin-right: 2%;
+    }
+    .cancel {
+    margin-right: 15%;
+    }
+</style>

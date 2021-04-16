c2a05c0b986ea57e1cf2c47bfcc4a6e74fb11940
qwark97 <marcin.plata1.konta@gmail.com>
2021-04-16 19:03:19 +0200

    add basics for new grocery list component

--- src/components/Groceries/NewGroceryList.vue
+++ src/components/Groceries/NewGroceryList.vue
@@ -1,8 +1,36 @@
 <template>
   <div>
-    <div>
-        <h1>works</h1>
-    </div>
+    <form>
+      <div>
+        <label for="name">List name</label>
+        <input type="text" v-model="name" id="name">
+      </div>
+      <div>
+        <label for="date">Date</label>
+        <input type="date" v-model="date" id="date">
+      </div>
+      <div>
+        <label for="products">Select bought product:</label>
+        <select class="form-select" v-model="products" id="products" @change="selectProd($event)">
+            <option disabled value="">Select product:</option>
+            <option v-for="product in allProducts" :key="product.name">{{product.name}}</option>
+        </select>
+        <div>
+          <p>Selected products:</p>
+          <ul>
+          <li v-for="product in selectedProducts" :key="product"> 
+            <span>{{ product.name }} - {{ product.price }}zł</span>
+            <span>
+              <div>
+                <input type="number" v-model="product.quantity">
+              </div>
+            </span>
+          </li>
+        </ul>
+        </div>
+      </div>
+
+    </form>
     <router-link to="/groceries">
         <NewGroceryListButton class="add" @click="addList()" :text="addListButtonText"/>
     </router-link>
@@ -13,6 +41,7 @@
 </template>
 
 <script>
+import {api} from '../../api/api.js'
 import NewGroceryListButton from "./NewGroceryListButton";
 
 export default {
@@ -23,14 +52,34 @@ export default {
   data: function() {
       return {
           addListButtonText: "Add",
-          cancelListButtonText: "Cancel"
+          cancelListButtonText: "Cancel",
+          allProducts: [],
+          selectedProducts: [],
       }
   },
   methods: {
     addList() {
-      console.log('works')
+      console.log(this.name)
+      console.log(this.date)
+      let d = new Date(this.date).getTime()
+      console.log(d)
+    },
+    selectProd(event) {
+      console.log(event.target.value)
+      this.selectedProducts.push(event.target.value)
     }
-  }
+  },
+  mounted: async function () {
+    this.loading = true;
+    this.error = null;
+    try {
+      this.allProducts= await api.getProducts();
+      this.loading = false;
+    } catch (error) {
+      this.loading = false;
+      this.error = error;
+    }
+  },
 }
 </script>
 

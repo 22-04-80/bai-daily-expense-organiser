f5062d2e0ce58e363aadde44b14ddc1ffbc806a5
qwark97 <marcin.plata1.konta@gmail.com>
2021-04-18 18:04:24 +0200

    add generic component as a button

---
+++ src/components/FloatingActionButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="floatingActionButton btn btn-primary btn-lg">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "FloatingActionButton",
+  props: ['text']
+};
+</script>
+
+<style scoped>
+.floatingActionButton {
+  position: fixed;
+  bottom: 8px;
+  right: 8px;
+}
+</style>
